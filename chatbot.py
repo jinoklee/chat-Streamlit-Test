dcb19c87c33242d936d633777d8bbe8a3fccc02e
jinoklee <jinoklee.01@gmail.com>
2024-10-21 22:54:29 +0900

    add

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.llms import OpenAI
 st.set_page_config(page_title="🦜🔗 Quickstart App")
 st.title('🦜🔗 Quickstart App')
 
